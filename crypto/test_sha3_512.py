de121bc4da6bef021036ce18e29ba7bdb564d7a6
Looyyd <filip.a.niedzielski@gmail.com>
2022-11-25 02:09:07 +0100

    State to 3D array and back to 1D array is ok, needed for function f

--- crypto/test_sha3_512.py
+++ crypto/test_sha3_512.py
@@ -6,20 +6,28 @@ def test_padding():
     input = "A"*71
     input_bytes = bytearray()
     input_bytes.extend(map(ord, input))
-    hash1 = Sha3_512(input_bytes)
+    hash1 = pad(input_bytes)
 
     input = "A"*72
     input_bytes = bytearray()
     input_bytes.extend(map(ord, input))
-    hash2 = Sha3_512(input_bytes)
+    hash2 = pad(input_bytes)
 
     input = "A"*73
     input_bytes = bytearray()
     input_bytes.extend(map(ord, input))
-    hash3 = Sha3_512(input_bytes)
+    hash3 = pad(input_bytes)
     assert len(hash1)%byte_rate == 0
     assert len(hash2)%byte_rate == 0
     assert len(hash3)%byte_rate == 0
 
 
 
+
+def test_1Dto3Dto1D():
+    test_state='A'*(1600//8)
+    input_bytes = bytearray()
+    input_bytes.extend(map(ord, test_state))
+    state3D= array_1Dto3D(input_bytes)
+    state = array_3Dto1D(state3D)
+    assert state == input_bytes
