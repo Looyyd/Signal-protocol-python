7854b40cef222e32463ab83f64054c1d33069155
DeltaT18 <bastien.thebaud@orange.fr>
2022-12-20 22:35:31 +0100

    small_comments

--- crypto/rsa.py
+++ crypto/rsa.py
@@ -29,6 +29,7 @@ def test_primality(n):
     #We will use 20 rounds of Rabin-Miller test
     
     #A list of the first primes to test our number
+    #TODO Keeping first prime test or direct Rabin-Miller ?
     small_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349]
     
     #Test first prime
@@ -77,7 +78,7 @@ def inv_mod(e,n):
         u0 = u0 + n
     return u0
 
-# This function generates public and associated private key
+# This function generates public and associated private keys
 def generate_keys():
 
     p = large_prime_number()
